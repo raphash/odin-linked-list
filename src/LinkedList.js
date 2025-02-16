24b00a849047a659ef222e4414811e60efba01ca
raphash <175216103+raphash@users.noreply.github.com>
2025-02-16 01:03:54 -0300

    feat: LinkedList prepend method
    This method adds a node in the LinkedList start,
    and sets it node to the head of the LinkedList.

--- src/LinkedList.js
+++ src/LinkedList.js
@@ -19,4 +19,21 @@ export class LinkedList {
     this.tail.next = node;
     this.tail = node;
   }
+
+  prepend(node) {
+    if (!this.head) {
+      this.head = node;
+      return;
+    }
+
+    if (this.head && !this.tail) {
+      this.tail = this.head;
+      this.head = node;
+      this.head.next = this.tail;
+      return;
+    }
+
+    node.next = this.head;
+    this.head = node;
+  }
 }
